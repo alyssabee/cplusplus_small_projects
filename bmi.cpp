b82634d6bfeb7367df3991b9435b22c737eacdb3
alyssabee <celicara29@gmail.com>
2016-10-08 03:30:14 -0400

    Edited bmi.cpp

--- bmi.cpp
+++ bmi.cpp
@@ -7,6 +7,7 @@
 int main()
 {
     double height = 0.0, weight = 0.0, bmi = 0.0;
+    std::string bmirange = "";
 
     std::cout << std::endl << "Please enter your weight in pounds: "; 
     std::cin >> weight; 
@@ -19,9 +20,19 @@ int main()
     weight = weight * 0.45;
     // Calculate BMI (kgs / m^2) 
     bmi = weight / (height * height);
+    // Calculate BMI range
+    if (bmi < 18.5) {
+        bmirange = "Underweight";
+    } else if (bmi >= 18.5 && bmi < 25) {
+        bmirange = "Normal weight";
+    } else if (bmi >= 25 && bmi < 30) {
+        bmirange = "Overweight";
+    } else if (bmi > 30) {
+        bmirange = "Obese";
+    }
 
-    std::cout << std::endl << "Your BMI is: " << bmi << std::endl;
-
+    std::cout << std::endl << "Your BMI is: " << bmi 
+              << " (" << bmirange << ")" << std::endl;
     return 0; 
     
 }
